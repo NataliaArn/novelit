974da9e7c64cf0190089e501a51ecebf6768c3ad
Natalia <nataliarnandis@gmail.com>
2024-11-19 20:38:21 +0100

    fix: sustituir bcrypt por bcryptjs

--- app/api/auth/signup/route.js
+++ app/api/auth/signup/route.js
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-import bcrypt from "bcrypt";
-import prisma from "../../../../lib/prisma";
-
+import bcrypt from "bcryptjs";
+import prisma from "@/lib/prisma";
 export async function POST(req) {
   try {
     const { username, email, password } = await req.json();
