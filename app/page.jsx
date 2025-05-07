fe139f5d415f85fc3d9cff782741c0cfcbbede03
Natalia <nataliarnandis@gmail.com>
2025-05-07 19:50:42 +0200

    fix: main page

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,9 @@
 "use client";
-import Header from "@/app/components/Header";
 import RecentNovels from "@/app/components/RecentNovels";
 
 export default function HomePage() {
   return (
     <div className="min-h-screen flex flex-col">
-      <Header />
       <main className="flex-1 p-6 bg-gray-100">
         <h1 className="text-2xl font-bold text-center mb-6">
           Novelas Recientes
